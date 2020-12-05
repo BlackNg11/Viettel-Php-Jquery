a9d66899ef94b96be6f7723c33710a8b965e5b71
Qphuc <tqp1610@gmail.com>
2020-12-05 20:28:15 +0700

    update csdl

--- csdl-dichvu.sql
+++ csdl-dichvu.sql
@@ -20,7 +20,7 @@ modifieddate timestamp null,
 createdby varchar(255) null,
 modifiedby varchar(255) null
 );
-create table khachhang_dangnhap (
+create table khachhang (
 id bigint not null primary key auto_increment,
 username varchar (150) not null,
 password varchar (150) not null,
@@ -30,6 +30,18 @@ diachi varchar (255) not null,
 dichvudangky varchar (255) not null,
 content text null,
 status int not null,
+createddate timestamp null,
+modifieddate timestamp null,
+createdby varchar(255) null,
+modifiedby varchar(255) null
+);
+create table news (
+id bigint not null primary key auto_increment,
+title varchar(255) null,
+thumbnail varchar(150) null,
+recomment text null,
+descripsion varchar(150) not null,
+content text not null,
 createddate timestamp null,
 modifieddate timestamp null,
 createdby varchar(255) null,
