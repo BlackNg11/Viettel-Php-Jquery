06141e16add1f0591f4f25653b4c879bb0785966
Qphuc <tqp1610@gmail.com>
2020-12-12 19:28:47 +0700

    update csdl

--- csdl-dichvu.sql
+++ csdl-dichvu.sql
@@ -47,3 +47,26 @@ modifieddate timestamp null,
 createdby varchar(255) null,
 modifiedby varchar(255) null
 );
+create table kho (
+id bigint not null primary key auto_increment,
+tenkho varchar(150) not null
+);
+create table loaisim (
+id bigint not null primary key auto_increment,
+tenloaisim varchar(150) not null
+);
+create table sim (
+id bigint not null primary key auto_increment,
+sdt int not null,
+gianhap int not null,
+giaban int not null,
+status int not null,
+idloaisim bigint not null,
+idkho bigint not null,
+createddate timestamp null,
+modifieddate timestamp null,
+createdby varchar(255) null,
+modifiedby varchar(255) null
+);
+alter table sim add constraint fk_sim_loaisim foreign key(idloaisim) references loaisim(id);
+alter table sim add constraint fk_user_role foreign key(idkho) references kho(id);
